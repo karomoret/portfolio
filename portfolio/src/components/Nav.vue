11cc0d7c9146a4813a00d40ab9f1dd41a037b09e
Jero <jero@moyosa.nl>
2025-09-29 16:03:02 +0200

    Simple navigation

---
+++ portfolio/src/components/Nav.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <a href="#/">Home</a>
+    <a href="#/about">About</a>
+    <a href="#/contact">Contact</a>
+    <a href="#/media">Media</a>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ portfolio/src/layouts/Main.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import Home from '../pages/Home.vue'
+import About from '../pages/About.vue'
+import Contact from '../pages/Contact.vue'
+import Media from '../pages/Media.vue'
+import { type Component, computed, ref } from 'vue'
+import Nav from '@/components/Nav.vue'
+
+const routes: { [key: string]: Component } = {
+  '/': Home,
+  '/about': About,
+  '/contact': Contact,
+  '/media': Media,
+}
+
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+  return routes[currentPath.value.substring(1)]
+})
+</script>
+
+<template>
+  <Nav />
+  <component :is="currentView"></component>
+</template>
+
+<style scoped></style>
