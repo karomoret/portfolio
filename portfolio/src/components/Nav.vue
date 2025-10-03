1a5bd1c67a69227c06dfb341c6da92d591dba2f8
Jero <jero@moyosa.nl>
2025-10-03 16:25:25 +0200

    Nav style

--- portfolio/src/components/Nav.vue
+++ portfolio/src/components/Nav.vue
@@ -2,11 +2,33 @@
 
 <template>
   <div class="container">
-    <a href="#/">Home</a>
-    <a href="#/about">About</a>
-    <a href="#/contact">Contact</a>
-    <a href="#/media">Media</a>
+    <h1 class="name">Karo Moret</h1>
+    <div class="links">
+      <a href="/about">About</a>
+      <a href="/media">Media</a>
+      <a href="/contact">Contact</a>
+    </div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.container {
+  padding: 1rem;
+  background-image: url('/images/nav.webp');
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  .name {
+    margin: 0 0 1rem 0;
+    letter-spacing: 0.4rem;
+    font-family: 'Rafika', sans-serif;
+  }
+
+  .links {
+    display: flex;
+    gap: 1rem;
+    font-family: 'Rafika', sans-serif;
+    color: lightgray;
+  }
+}
+</style>
